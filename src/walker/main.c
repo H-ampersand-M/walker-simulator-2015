822f1c833fd0fd278b7ae2052aa40b66a8625bdf
Harenome Ranaivoarivony Razanajato <razanajato@etu.unistra.fr>
2015-03-31 15:50:01 +0200

    use request.h and active.h in main.c

--- src/walker/main.c
+++ src/walker/main.c
@@ -47,261 +47,199 @@
 #include <time.h>
 #include <unistd.h>
 #include <float.h>
-
-/* #ifndef _POSIX_PRIORITY_SCHEDULING */
-/*     #error "No POSIX priority scheduling." */
-/* #endif */
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <errno.h>
 
 #include "walker/version.h"
+#include "walker/active.h"
+#include "walker/request.h"
 
+#define TIMER_SIG SIGRTMIN
 #define NOTIFY_SIG SIGRTMAX
 
-static const int MAX_MESSAGES = 256;
+#define SECURITY_DURATION 3
+#define MINIMUM_DURATION 3
+#define OPTIONAL_DURATION 5
+
 timer_t timer;
-static pid_t other;
-static mqd_t queue;
-static const char * QUEUE_NAME = "/walker_texas_ranger";
+pid_t other;
 
-typedef struct way_request
+static bool set_sigaction (int signal, void (* action) (int, siginfo_t *, void *))
 {
-    int integer;
-    struct timespec date;
-} way_request;
+    struct sigaction sa;
+    sa.sa_sigaction = action;
+    sa.sa_flags = SA_SIGINFO;
+    sigemptyset (& sa.sa_mask);
 
-static int current = 0;
-static mqd_t current_queue;
+    int success = sigaction (signal, & sa, NULL);
 
-static way_request requests[3] =
-{
-    {0, {0,0}},
-    {0, {0,0}},
-    {0, {0,0}},
-};
+    if (success == -1)
+        perror ("sigaction");
 
-static void current_changed (int a, siginfo_t * b, void * c)
-{
-    (void) a; (void) b; (void) c;
-    int prio = 0;
-    if (mq_receive (current_queue, & current, sizeof current, & prio) == -1)
-    {
-        perror ("mq_receive");
-        fprintf (stderr, "current_changed eats ass!!!!\n");
-    }
-
-    struct sigevent sev;
-    sev.sigev_notify = SIGEV_SIGNAL;
-    sev.sigev_signo = NOTIFY_SIG;
-    sev.sigev_value.sival_ptr = & current_queue;
-    mq_notify (current_queue, & sev);
-
-    printf ("Current: %d\n", current);
+    return success != -1;
 }
 
-static void show_requests (void)
+static bool wlk_queue_notify (mqd_t queue, int signo)
 {
-    for (size_t i = 0; i < 3; ++i)
-    {
-        way_request r = requests[i];
-        fprintf (stderr, "[%u]: %d %d %d\n", i, r.integer, r.date.tv_sec, r.date.tv_nsec);
-    }
+    struct sigevent event;
+
+    event.sigev_notify = SIGEV_SIGNAL;
+    event.sigev_signo = signo;
+    event.sigev_value = (union sigval) { .sival_int = 0, };
+    int success = mq_notify (queue, & event);
+
+    if (success == -1)
+        perror ("mq_notify");
+
+    return success != -1;
+}
+
+static void init_timer (int signal)
+{
+    struct sigevent event;
+    event.sigev_notify = SIGEV_SIGNAL;
+    event.sigev_signo = signal;
+    event.sigev_value = (union sigval) { .sival_int = 0, };
+
+    timer_create (CLOCK_REALTIME, & event, & timer);
+}
+
+static void set_one_shot_timer (time_t seconds)
+{
+    struct itimerspec timer_spec;
+    timer_spec.it_value.tv_sec = seconds;
+    timer_spec.it_value.tv_nsec = 0;
+    timer_spec.it_interval.tv_sec = 0;
+    timer_spec.it_interval.tv_nsec = 0;
+
+    timer_settime (timer, 0, & timer_spec, NULL);
+}
+
+static void activity_change (int a, siginfo_t * b, void * c)
+{
+    (void) a; (void) b; (void) c;
+
+    wlk_queue_notify (wlk_activity_queue (), NOTIFY_SIG);
+    wlk_receive_activity ();
+    wlk_debug_activity ();
 }
 
 static void walker_interface (void)
 {
-    int route = 0;
-
-    struct sigevent sev;
-    sev.sigev_notify = SIGEV_SIGNAL;
-    sev.sigev_signo = NOTIFY_SIG;
-    sev.sigev_value.sival_ptr = & current_queue;
-    mq_notify (current_queue, & sev);
-
-    struct sigaction sa;
-    sa.sa_sigaction = current_changed;
-    sa.sa_flags = SA_SIGINFO;
-    sigemptyset (& sa.sa_mask);
-    sigaction (NOTIFY_SIG, & sa, NULL);
-
     for (;;)
     {
-        printf ("> ");
-        if (scanf ("%d", & route) == 1)
-        {
-            struct timespec now;
-            clock_gettime (CLOCK_REALTIME, & now);
+        printf ("\x1B[1m%lu\x1B[0m \x1B[37m>> \x1B[0m", wlk_get_active_way ());
 
-            struct way_request r = { route, now };
+        unsigned int route = 0;
+        errno = 0;
+        int scan = scanf ("%u", & route); scanf ("%*[^\n]%*[\n]");
 
-            mq_send (queue, (char *) & r, sizeof r, 0);
-        }
+        if (errno == EINTR)
+            #ifdef DEBUG
+            fprintf (stderr, "\x1B[1m\x1B[38;5;220mwalker_interface (): "
+                    "`scanf (\"%%u%%*[^\\n]%%*[\\n]\")` interrupted!\x1B[0m\n");
+            #else
+            printf ("\n");
+            #endif
+
+        if (scan == 1)
+            wlk_send_request (route);
     }
 }
 
 static void security (void)
 {
-    /* Sigevent. */
-    struct sigevent event;
-    event.sigev_notify = SIGEV_SIGNAL;
-    event.sigev_signo = SIGRTMIN;
-    event.sigev_value = (union sigval) { .sival_int = 0, };
-
-    /* Timer information. */
-    struct itimerspec timer_spec;
-    timer_spec.it_value.tv_sec = 1;
-    timer_spec.it_value.tv_nsec = 0;
-    timer_spec.it_interval.tv_sec = 0;
-    timer_spec.it_interval.tv_nsec = 0;
-
-    /* Set the timer. */
-    timer_create (CLOCK_REALTIME, & event, & timer);
-    timer_settime (timer, 0, & timer_spec, NULL);
+    set_one_shot_timer (SECURITY_DURATION);
 
     /* Wait for the SIGRTMIN signals. */
     sigset_t set;
     sigfillset (& set);
     sigdelset (& set, SIGRTMIN);
     sigsuspend (& set);
-}
 
-static void peek_queue (void)
-{
-    struct mq_attr attr;
-    way_request r;
-    int prio = 0;
-    mq_getattr (queue, & attr);
-    for (long i = 0; i < attr.mq_curmsgs; ++i)
-    {
-        mq_receive (queue, (char *) & r, sizeof r, NULL);
-        int j = r.integer;
-        if (! requests[j].integer && current != j)
-        {
-            requests[r.integer].date = r.date;
-            requests[r.integer].integer = 1;
-        }
-    }
-}
-
-static bool has_request (void)
-{
-    bool has = false;
-    for (size_t i = 0; ! has && i < 3; ++i)
-    {
-        if (requests[i].integer)
-        {
-            fprintf (stderr, "has_requests: %lu, %d\n", i, requests[i].integer);
-            has = true;
-        }
-    }
-    return has;
+    wlk_set_active_way (0);
+    wlk_send_activity ();
 }
 
 static void set_next_request (void)
 {
-    /* show_requests (); */
-    if (has_request ())
-    {
-        fprintf (stderr, "set_next: has_request ?!\n");
-        struct timespec now;
-        clock_gettime (CLOCK_REALTIME, & now);
+    size_t next;
 
-        int selected = 0;
-        double min = DBL_MAX;
-        double now_d = ((double) now.tv_sec * 1e9 + (double) now.tv_nsec);
-        for (size_t i = 0; i < 3; ++i)
-        {
-            if (requests[i].integer)
-            {
-                double t = ((double) requests[i].date.tv_sec * 1e9 + (double) requests[i].date.tv_nsec);
-                if (now_d - t < min)
-                {
-                    min = now_d - t;
-                    selected = i;
-                }
-            }
-        }
+    wlk_debug_requests ();
 
-        current = selected;
-        printf ("selected: %d; current: %d\n", selected, current);
-    }
+    if (wlk_request_pending ())
+        next = wlk_pop_request ();
     else
-        current = (current + 1) % 3;
+        next = wlk_round_robin ();
 
-    fprintf (stderr, "||| current %d\n", current);
-    int prio = SIGRTMIN;
-    if (mq_send (current_queue, & current, sizeof current, prio) == -1)
-    {
-        {
-            perror ("mq_send");
-            fprintf (stderr, "alqsdfjlqsdfj\n");
-        }
-    }
-    fprintf (stderr, "||| current %d\n", current);
+    wlk_set_active_way (next);
+    wlk_send_activity ();
 }
 
 static void request_pending (int a, siginfo_t * b, void * c)
 {
     (void) a; (void) b; (void) c;
 
-    struct itimerspec timer_spec;
-    timer_spec.it_value.tv_sec = 0;
-    timer_spec.it_value.tv_nsec = 0;
-    timer_spec.it_interval.tv_sec = 0;
-    timer_spec.it_interval.tv_nsec = 0;
-
-    timer_settime (timer, 0, & timer_spec, NULL);
-
-    peek_queue ();
+    set_one_shot_timer (0);
+    wlk_queue_notify (wlk_request_queue (), NOTIFY_SIG);
+    wlk_pull_requests (wlk_get_active_way ());
 }
 
 static void way (void)
 {
-    fprintf (stderr, "way start\n");
-
-    /* Sigevent. */
-    struct sigevent event;
-    event.sigev_notify = SIGEV_SIGNAL;
-    event.sigev_signo = SIGRTMIN;
-    event.sigev_value = (union sigval) { .sival_int = 0, };
-
-    /* Timer information. */
-    struct itimerspec timer_spec;
-    timer_spec.it_value.tv_sec = 3;
-    timer_spec.it_value.tv_nsec = 0;
-    timer_spec.it_interval.tv_sec = 0;
-    timer_spec.it_interval.tv_nsec = 0;
-
-    /* Set the timer. */
-    timer_create (CLOCK_REALTIME, & event, & timer);
-    timer_settime (timer, 0, & timer_spec, NULL);
+    set_one_shot_timer (MINIMUM_DURATION);
 
     /* Wait for the SIGRTMIN signals. */
     sigset_t set;
     sigfillset (& set);
     sigdelset (& set, SIGRTMIN);
+    sigdelset (& set, SIGINT);
     sigprocmask (SIG_SETMASK, & set, NULL);
     sigsuspend (& set);
 
-    fprintf (stderr, "way 7sec end\n");
     ///////////////////////////////////////////
 
     sigdelset (& set, NOTIFY_SIG);
-    peek_queue ();
-    if (! has_request ())
-    {
-        fprintf (stderr, "way 23sec start\n");
-        timer_spec.it_value.tv_sec = 5;
-        timer_settime (timer, 0, & timer_spec, NULL);
-        sigsuspend (& set);
-        fprintf (stderr, "way 23sec end (why ??????????)\n");
-    }
-    sigprocmask (SIG_SETMASK, & set, NULL);
 
-    requests[current].integer = 0;
+    wlk_pull_requests (wlk_get_active_way ());
+    if (! wlk_request_pending ())
+    {
+        set_one_shot_timer (OPTIONAL_DURATION);
+        sigsuspend (& set);
+    }
+
+    sigprocmask (SIG_SETMASK, & set, NULL);
+    wlk_reset_request (wlk_get_active_way ());
 }
 
 static void sigrtmin_action (int a, siginfo_t * b, void * c)
 {
     (void) a; (void) b; (void) c;
+    return;
+}
+
+static void sigint_action (int a, siginfo_t * b, void * c)
+{
+    (void) a; (void) b; (void) c;
+
+    wlk_close_request_queue ();
+    wlk_close_activity_queue ();
+
+    if (other)
+    {
+        kill (other, SIGINT);
+        wait (NULL);
+
+        mq_unlink ("/walker_request_queue");
+        mq_unlink ("/walker_activity_queue");
+
+        /* Erase the ^C. */
+        fprintf (stderr, "\b\b  \n");
+        /* fprintf (stderr, "Bye!\n"); */
+    }
+
+    exit (EXIT_SUCCESS);
 }
 
 /**
@@ -321,61 +259,19 @@ int main (int argc, char ** argv)
     }
 
     fprintf_version (stderr, "Walker Simulator 2015");
-    /* fprintf (stderr, "This program does not do anything yet.\n"); */
 
-    /* Open the queue. */
-    struct mq_attr attrs;
-    attrs.mq_flags = 0;
-    attrs.mq_msgsize = sizeof (way_request);
-    attrs.mq_maxmsg = 5;
-    attrs.mq_curmsgs = 0;
-    mq_unlink (QUEUE_NAME);
-    queue = mq_open(QUEUE_NAME, O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR,
-            & attrs);
+    set_sigaction (SIGINT, sigint_action);
 
-    if (queue == -1)
-    {
-        fprintf (stderr, "%s\n", QUEUE_NAME);
-        perror ("mq_open");
-        exit (1);
-    }
-
-    attrs.mq_flags = 0;
-    attrs.mq_msgsize = sizeof current;
-    attrs.mq_maxmsg = 5;
-    attrs.mq_curmsgs = 0;
-    mq_unlink ("/walker_current");
-    current_queue = mq_open("/walker_current", O_RDWR | O_CREAT | O_EXCL, S_IRUSR | S_IWUSR,
-            & attrs);
-
-    if (current_queue == -1)
-    {
-        fprintf (stderr, "%s\n", "/walker_current");
-        perror ("mq_open");
-        exit (1);
-    }
-
+    wlk_open_request_queue ();
+    wlk_open_activity_queue ();
 
     other = fork ();
     if (other)
     {
-        struct sigevent sev;
-        sev.sigev_notify = SIGEV_SIGNAL;
-        sev.sigev_signo = NOTIFY_SIG;
-        sev.sigev_value.sival_ptr = & queue;
-        mq_notify (queue, & sev);
-
-        struct sigaction sa;
-        sa.sa_sigaction = request_pending;
-        sa.sa_flags = SA_SIGINFO;
-        sigemptyset (& sa.sa_mask);
-        sigaction (NOTIFY_SIG, & sa, NULL);
-
-        /* Set the SIGRTMIN behaviour. */
-        sa.sa_sigaction = sigrtmin_action;
-        sa.sa_flags = SA_SIGINFO;
-        sigemptyset (& sa.sa_mask);
-        sigaction (SIGRTMIN, & sa, NULL);
+        init_timer (TIMER_SIG);
+        wlk_queue_notify (wlk_request_queue (), NOTIFY_SIG);
+        set_sigaction (NOTIFY_SIG, request_pending);
+        set_sigaction (TIMER_SIG, sigrtmin_action);
 
         for (;;)
         {
@@ -386,6 +282,8 @@ int main (int argc, char ** argv)
     }
     else
     {
+        set_sigaction (NOTIFY_SIG, activity_change);
+        wlk_queue_notify (wlk_activity_queue (), NOTIFY_SIG);
         walker_interface ();
     }
 
